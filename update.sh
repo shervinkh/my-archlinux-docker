d6b3ee339271081ac673533a47155fcbbf5f0089
Shervin Khastoo <shervin@shervin.org>
2017-03-09 06:35:43 +0000

    Faster update.sh

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-pacman-key --refresh-keys && yes | pacman -Syu 2> /dev/null && pkgfile --update
+yes | pacman -Sy archlinux-keyring && yes | pacman -Syu 2> /dev/null && pkgfile --update
 
