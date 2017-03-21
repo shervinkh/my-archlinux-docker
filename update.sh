01175d4c1efbf56ef70cfc01f7ea8ea96b23f282
Shervin Khastoo <shervin@shervin.org>
2017-03-21 21:53:52 +0330

    Add force update

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-yes | pacman -Sy archlinux-keyring && yes | pacman -Syu 2> /dev/null && pkgfile --update
+yes | pacman -Sy archlinux-keyring && yes | pacman -Syu --force 2> /dev/null && pkgfile --update
 
